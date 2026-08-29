eef8b1e55f0c70848788addcfa4fd18a42014cc5
ph1958 <33202278+ph1958@users.noreply.github.com>
2026-08-29 12:13:53 -0700

    Advance Statamic content profiles

--- config/discussionbridge.php
+++ config/discussionbridge.php
@@ -9,7 +9,7 @@ return [
     'lane' => env('DISCUSSIONBRIDGE_LANE'),
     'collections' => array_values(array_filter(array_map('trim', explode(',', (string) env('DISCUSSIONBRIDGE_COLLECTIONS', 'pages'))))),
     'adapter_id' => 'statamic-discussionbridge',
-    'adapter_version' => '0.1.0-alpha.2',
+    'adapter_version' => '0.1.0-alpha.3',
     'connect_timeout_seconds' => 2,
     'response_timeout_seconds' => 5,
     'maximum_response_bytes' => 65536,
